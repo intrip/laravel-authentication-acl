785e6e3f2beed881b85b880176af0f3ae8c849fa
Jacopo <beschi.jacopo@gmail.com>
2014-04-08 01:28:23 +0200

    form change pass

--- src/views/client/auth/changepassword.blade.php
+++ src/views/client/auth/changepassword.blade.php
@@ -1,19 +1,38 @@
 @extends('authentication::admin.layouts.baseauth')
+@section('title')
+Change password
+@stop
 @section('container')
-    <h1>Modifica password</h1>
-    <?php $message = Session::get('message'); ?>
-    @if( isset($message) )
-        <div class="alert alert-success">{{$message}}</div>
-    @endif
-    @if($errors && ! $errors->isEmpty() )
-        @foreach($errors->all() as $error)
+<div class="row centered-form">
+    <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h3 class="panel-title">Change your password to {{Config::get('authentication::app_name')}}</h3>
+            </div>
+            @if($errors && ! $errors->isEmpty() )
+            @foreach($errors->all() as $error)
             <div class="alert alert-danger">{{$error}}</div>
-        @endforeach
-    @endif
-    {{Form::open(array('url' => URL::action("Jacopo\Authentication\Controllers\AuthController@postChangePassword"), 'method' => 'post') )}}
-            {{FormField::password(array('label' => 'password' ))}}
-            {{Form::hidden('email',$email)}}
-            {{Form::hidden('token',$token)}}
-            {{Form::submit('Cambia', array("class"=>"btn btn-large btn-info"))}}
-    {{Form::close()}}
+            @endforeach
+            @endif
+            <div class="panel-body">
+                {{Form::open(array('url' => URL::action("Jacopo\Authentication\Controllers\AuthController@postChangePassword"), 'method' => 'post') )}}
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            {{Form::label('password', 'New password: ')}}
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                                {{Form::password('password', ['id' => 'password', 'class' => 'form-control', 'placeholder' => 'New password', 'required', 'autocomplete' => 'off'])}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <input type="submit" value="Change password" class="btn btn-info btn-block">
+                {{Form::hidden('email',$email)}}
+                {{Form::hidden('token',$token)}}
+                {{Form::close()}}
+            </div>
+        </div>
+    </div>
+</div>
 @stop
