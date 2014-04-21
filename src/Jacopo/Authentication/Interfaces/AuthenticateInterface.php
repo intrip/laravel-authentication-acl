19f2f9c979b925eb29fc6eae080cc231a3e6a7df
Jacopo <beschi.jacopo@gmail.com>
2014-04-21 15:24:50 +0200

    method added to auth interface

--- src/Jacopo/Authentication/Interfaces/AuthenticateInterface.php
+++ src/Jacopo/Authentication/Interfaces/AuthenticateInterface.php
@@ -2,47 +2,65 @@
 
 interface AuthenticateInterface
 {
-    /**
-     * Force authentication on a user
-     * @param $credentials
-     * @param $remember
-     * @return mixed
-     */
-    public function authenticate($credentials, $remember);
+  /**
+   * Force authentication on a user
+   *
+   * @param $credentials
+   * @param $remember
+   * @return mixed
+   */
+  public function authenticate($credentials, $remember);
 
-    /**
-     *
-     * @param $user
-     * @param $remember
-     * @return mixed
-     */
-    public function loginById($id, $remember);
+  /**
+   * @param $user
+   * @param $remember
+   * @return mixed
+   */
+  public function loginById($id, $remember);
 
-    /**
-     * Logout
-     * @return mixed
-     */
-    public function logout();
+  /**
+   * Logout
+   *
+   * @return mixed
+   */
+  public function logout();
 
-    /**
-     * @return mixed
-     */
-    public function getErrors();
+  /**
+   * @return mixed
+   */
+  public function getErrors();
 
-    /**
-     * Obtain the user with his email
-     *
-     * @param $email
-     * @return mixed
-     * @throws \Jacopo\Authentication\Exceptions\UserNotFoundException
-     * @return mixed
-     */
-    public function getUser($email);
+  /**
+   * Obtain the user with his email
+   *
+   * @param $email
+   * @return mixed
+   * @throws \Jacopo\Authentication\Exceptions\UserNotFoundException
+   * @return mixed
+   */
+  public function getUser($email);
 
-    /**
-     * Gets the user activaction token
-     * @param $email
-     * @return String
-     */
-    public function getActivationToken($email);
+  /**
+   * Gets the user activaction token
+   *
+   * @param $email
+   * @return String
+   */
+  public function getActivationToken($email);
+
+  /**
+   * Obtains a user given his user id
+   *
+   * @param $id
+   * @return mixed
+   */
+  public function getUserById($id);
+
+  /**
+   * Obtain the current logged user
+   *
+   * @return mixed
+   */
+  public function getLoggedUser();
+
 }
--- src/config/database.php
+++ src/config/database.php
@@ -13,7 +13,7 @@ return array(
         'mysql' => array(
             'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => 'authentication',
+            'database'  => 'database',
             'username'  => 'root',
             'password'  => '',
             'charset'   => 'utf8',
