0632fd8ec7b8219adde2a3b609536e0554e238ac
Jacopo <beschi.jacopo@gmail.com>
2014-10-07 23:57:08 +0200

    init codeception acceptance

---
+++ tests/_support/AcceptanceHelper.php
@@ -0,0 +1,15 @@
+<?php
+namespace Codeception\Module;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class AcceptanceHelper extends \Codeception\Module
+{
+  public function _beforeSuite($settings = array()) {
+    $this->debug('MIGRATING BEFORE RUN');
+    $I = $this->getModule('Laravel4');
+    $artisan = $I->grabService('artisan');
+    $artisan->call('migrate');
+  }
+}
