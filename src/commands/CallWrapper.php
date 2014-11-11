d6dcecbe92308f1060c45cb27da2cb7116c47234
Ryan McAllen <rhyno@rhynodesigns.com>
2014-11-11 11:33:37 -0500

    Consistent tab width & line spacing

--- src/commands/CallWrapper.php
+++ src/commands/CallWrapper.php
@@ -1,15 +1,17 @@
 <?php
-class CallWrapper
-{
-  protected $wrapper_obj;
 
-  function __construct($wrapped_obj)
-  {
-    $this->wrapped_obj = $wrapped_obj;
-  }
+class CallWrapper {
 
-  public function __call($name, $params)
-  {
-    return call_user_func_array([$this->wrapped_obj, $name], $params);
-  }
-} 
+    protected $wrapper_obj;
+
+    function __construct($wrapped_obj)
+    {
+        $this->wrapped_obj = $wrapped_obj;
+    }
+
+    public function __call($name, $params)
+    {
+        return call_user_func_array([$this->wrapped_obj, $name], $params);
+    }
+
+}
