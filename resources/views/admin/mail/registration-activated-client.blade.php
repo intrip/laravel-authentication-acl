331226ebcd62a6369143fee9b1985806b80fccb7
jbond3 <mhillmarcus@yahoo.co.uk>
2016-07-09 17:37:47 +0100

    Update registration-activated-client.blade.php
    succesfully -> successfully

--- resources/views/admin/mail/registration-activated-client.blade.php
+++ resources/views/admin/mail/registration-activated-client.blade.php
@@ -10,7 +10,7 @@
 <h2>Welcome to {!! Config::get('acl_base.app_name')!!}</h2>
 <div>
     <h3>Dear: {!! $body['email'] !!}</h3>
-    <strong>Your email has been confirmed succesfully.</strong>
+    <strong>Your email has been confirmed successfully.</strong>
     You can now login to the website using the
     <a href="{!! URL::to('/login') !!}">Following link</a>.
 </div>
